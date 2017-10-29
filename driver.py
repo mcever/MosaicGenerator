0dc35e9eafbb0578630271ca278de8fedd8cfc90
A <austinmcever@169-231-118-108.wireless.ucsb.edu>
2017-10-29 16:40:25 -0700

    merge

--- driver.py
+++ driver.py
@@ -11,9 +11,5 @@ args = parser.parse_args()
 img1 = cv2.imread(args.img1, 0) # read in images in greyscale
 img2 = cv2.imread(args.img2, 0)
 
-cv2.imshow("Img1", img1)
-cv2.imshow("Img2", img2)
-cv2.waitKey(0)
-
 M = Mosaic()
 M.create(img1, img2)
